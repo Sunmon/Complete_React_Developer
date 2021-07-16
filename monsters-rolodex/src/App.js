1ce7a7f7d038a370d79ae14080beb9884617954c
Sunmon <blackmilk274@gmail.com>
2021-07-16 16:27:42 +0900

    [feat] card 구현 및 검색 기능 구현

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -1,26 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
+/* eslint-disable react/no-unused-state */
+/* eslint-disable react/destructuring-assignment */
+import React, { Component } from 'react';
+// eslint-disable-next-line no-unused-vars
+import { CardList } from './components/card-list/card-list.componet';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      monsters: [],
+      searchField: '',
+    };
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(users => this.setState({ monsters: users }));
+  }
+
+  render() {
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter(monster =>
+      monster.name.toLowerCase().includes(searchField.toLowerCase()),
+    );
+
+    return (
+      <div className="App">
+        <input
+          type="search"
+          placeholder="search monsters"
+          onChange={e => this.setState({ searchField: e.target.value })}
+        />
+        <CardList monsters={filteredMonsters} />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ monsters-rolodex/src/components/card-list/card-list.componet.jsx
@@ -0,0 +1,15 @@
+/* eslint-disable import/prefer-default-export */
+import React from 'react';
+import { Card } from '../card/card.component';
+import './card-list.styles.css';
+
+export const CardList = props => {
+  const { monsters } = props;
+  return (
+    <div className="card-list">
+      {monsters.map(monster => (
+        <Card key={monster.id} monster={monster} />
+      ))}
+    </div>
+  );
+};
