55916dff3d4d6587d444c8fe4631f7de136687b1
Sunmon <blackmilk274@gmail.com>
2021-07-18 22:50:39 +0900

    [feat] update menu-items to navigate withRouter

--- crwn-clothing/src/App.js
+++ crwn-clothing/src/App.js
@@ -1,9 +1,26 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
 import HomePage from './pages/hompage/homepage.component';
+
 import './App.css';
 
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>
+);
+
 function App() {
-  return <HomePage></HomePage>;
+  return (
+    <div>
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/hats" component={HatsPage} />
+      </Switch>
+    </div>
+  );
+
+  // return <HomePage></HomePage>;
   // return <div>Hello world</div>;
 }
 
