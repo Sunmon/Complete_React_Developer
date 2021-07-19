ceb3544d4d6b6bc5f6dfc9b24f4820b280be635a
Sunmon <blackmilk274@gmail.com>
2021-07-19 18:21:28 +0900

    [feat] collection item & shop page

--- crwn-clothing/src/App.js
+++ crwn-clothing/src/App.js
@@ -1,27 +1,19 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import HomePage from './pages/hompage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
 
 import './App.css';
-
-const HatsPage = () => (
-  <div>
-    <h1>HATS PAGE</h1>
-  </div>
-);
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path="/" component={HomePage} />
-        <Route path="/hats" component={HatsPage} />
+        <Route path="/shop" component={ShopPage} />
       </Switch>
     </div>
   );
-
-  // return <HomePage></HomePage>;
-  // return <div>Hello world</div>;
 }
 
 export default App;
