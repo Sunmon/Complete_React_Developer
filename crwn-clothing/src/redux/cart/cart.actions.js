4be48f92e2a0ed8f38bbf1af31e6057357c5c352
Sunmon <blackmilk274@gmail.com>
2021-07-29 21:13:46 +0900

    [feat] 장바구니에 같은 아이템 추가시 개수 증가

--- crwn-clothing/src/redux/cart/cart.actions.js
+++ crwn-clothing/src/redux/cart/cart.actions.js
@@ -4,3 +4,8 @@ export const toggleCartHidden = () => ({
   type: CartActionTypes.TOGGLE_CART_HIDDEN,
   //paload: 필요없음
 });
+
+export const addItem = item => ({
+  type: CartActionTypes.ADD_ITEM,
+  payload: item,
+});
