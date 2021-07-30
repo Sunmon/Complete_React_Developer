7d0d8ceaa439a57da3e26ab9401d87f41a59ff73
sunmon <blackmilk274@gmail.com>
2021-07-30 16:58:47 +0900

    [feat] checkout 페이지 추가 & checkout 버튼 클릭시 이동

--- crwn-clothing/src/components/cart-dropdown/cart-dropdown.component.jsx
+++ crwn-clothing/src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -4,9 +4,11 @@ import CustomButton from '../custom-button/custom-button.component';
 import CartItem from '../cart-item/cart-item.component';
 import { selectCartItems } from '../../redux/cart/cart.selectors';
 import { createStructuredSelector } from 'reselect';
+import { withRouter } from 'react-router';
+
 import './cart-dropdown.styles.scss';
 
-const CartDropdown = ({ cartItems }) => {
+const CartDropdown = ({ cartItems, history }) => {
   return (
     <div className="cart-dropdown">
       <div className="cart-items">
@@ -18,7 +20,9 @@ const CartDropdown = ({ cartItems }) => {
           <span className="empty-message">Your cart is empty </span>
         )}
       </div>
-      <CustomButton> GO To CHECKOUT </CustomButton>
+      <CustomButton onClick={() => history.push('/checkout')}>
+        GO To CHECKOUT
+      </CustomButton>
     </div>
   );
 };
@@ -27,4 +31,4 @@ const mapStateToProps = createStructuredSelector({
   cartItems: selectCartItems,
 });
 
-export default connect(mapStateToProps)(CartDropdown);
+export default withRouter(connect(mapStateToProps)(CartDropdown));
