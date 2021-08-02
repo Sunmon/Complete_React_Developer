03a118143917201811827c21cfa2b5cc6fe4d6c6
sunmon <blackmilk274@gmail.com>
2021-08-02 16:30:35 +0900

    persistence로 장바구니 데이터 영속성 적용

--- crwn-clothing/src/redux/root-reducer.js
+++ crwn-clothing/src/redux/root-reducer.js
@@ -1,9 +1,20 @@
 import { combineReducers } from 'redux';
+import { persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
 import userReducer from './user/user-reducer';
 import cartReducer from './cart/cart.reducer';
 
-export default combineReducers({
+const persistConfig = {
+  key: 'root', // store from the root
+  storage,
+  whitelist: ['cart'], // to do persist
+};
+
+const rootReducer = combineReducers({
   user: userReducer,
   cart: cartReducer,
 });
+
+// root reducer를 persist reducer로 변환하여 export
+export default persistReducer(persistConfig, rootReducer);
